458a5d170b7bebb5f3cca78e9c79e53673f3d057
carlosmig <45362243+carlosmig@users.noreply.github.com>
2021-05-28 17:09:55 -0400

    Add files via upload

--- OverallFC_methods.py
+++ OverallFC_methods.py
@@ -65,9 +65,125 @@ def linear_correction(Y,X):
     return(Y_res)    
     
 
+def permutation_test(Y1,Y2,permutations,parametric=True,paired=False):
+    
+    '''
+    Simple Permutation test to analyzed if the measured difference between two groups is statistical
+    significant. The permutation test randomly selects subjects from the two groups. After,
+    the statistic for between groups difference was computed. When repeating this procedure X number of
+    times, we can generate a cdf distribution of the statistic. Finally, we compare the real statistic with the
+    cdf distribution.
+    
+    Parameters
+    ---------
+    Y1 : numpy array.
+         network's metric of group 1. Matrix of n x m, with n variables and m observations.
+    Y2 : numpy array.
+         network's metric of group 2. Matrix of n x m, with n variables and m observations  
+    permutations : integer.
+                   number of permutations to perform.
+    parametric : boolean.
+                 True for using parametric tests (t test), False for non parametric tests
+                 (wilcoxon, mann-whitney).
+    paired : boolean.
+             True for paired tests, False for unpaired tests.
+             
+    Returns
+    ------
+    t_real : list.
+             It contains the real statistics for between-grops difference.
+    mu : list.
+         mean values of the surrogate statistics for each variable.
+    p_val : list.
+            p-values associated to the real statistic and the cdf distribution of surrogates.
+    surrogates_mean : list.
+                      arrays with the mean difference of metrics for each surrogate.
+    surrogates_std : list.
+                     arrays with the std of the difference for each surrogate.
+    '''
+    
+    
+    
+    ###Permutation test
+    variables = Y1.shape[0] #number of variables
+    statistic = np.zeros((variables,permutations)) #matrix for saving statistics values
+    surrogates_mean = np.zeros((variables,permutations)) #matrix with the mean difference of metrics for each surrogate
+    surrogates_std = np.zeros((variables,permutations)) #matrix with the std of the difference for each surrogate
+    observations = np.min([Y1.shape[1],Y2.shape[1]]) #number of subjects/observations
+    t_real = np.zeros(variables) #real statistic for variables
+    mu = np.zeros(variables) #mean value of the surrogate statistics for each variable
+    p_val = np.zeros(variables) #p-values for each variable
+    
+    #permutations
+    for i in range(0,permutations):
+        np.random.seed(i)
+        
+        #surrogate groups for variables
+        surr_Y1, surr_Y2 = np.zeros((variables,observations)), np.zeros((variables,observations))
+        
+        #Random selection of subjects from groups 1 and 2. Both surrogate groups have 50% of the subjects
+        #of the original data. Paired == True considered that surr_X1[i] and surr_X2[j] belongs to the 
+        #same subject. Paired == False completely randomize the groups.
+        
+        
+        surr_idx = np.arange(0,observations,1)
+        np.random.shuffle(surr_idx)
+        even = np.arange(0,observations,2)
+        odd = np.arange(1,observations,2)
+                    
+        #variables
+        surr_Y1[:,even] = Y1[:,surr_idx[even]]
+        surr_Y1[:,odd] = Y2[:,surr_idx[odd]]
+        surr_Y2[:,even] = Y2[:,surr_idx[even]]
+        surr_Y2[:,odd] = Y1[:,surr_idx[odd]]
 
-def permutation_test(Y1,Y2,X1,X2,permutations,iters,tolerance,
-                     parametric=True,paired=False):
+    
+        #Calculate the surrogate statistic for between-groups difference
+        for var in range(0,variables):
+        
+            if (parametric == True) &  (paired == False):
+                statistic[var,i] = stats.ttest_ind(surr_Y1[var,:], surr_Y2[var,:])[0]
+            elif (parametric == True) &  (paired == True):
+                statistic[var,i] = stats.ttest_rel(surr_Y1[var,:], surr_Y2[var,:])[0]
+            elif (parametric == False) &  (paired == False):
+                statistic[var,i] = stats.mannwhitneyu(surr_Y1[var,:], surr_Y2[var,:])[0]
+            elif (parametric == False) &  (paired == True):
+                statistic[var,i] = stats.wilcoxon(surr_Y1[var,:], surr_Y2[var,:])[0]            
+
+            surrogates_mean[var,i] = np.mean(surr_Y1[var,:]-surr_Y2[var,:])
+            surrogates_std[var,i] = np.std(surr_Y1[var,:]-surr_Y2[var,:])
+
+    #Calculate the real statistic for between-groups difference            
+    for var in range(0,variables):            
+       
+        if (parametric == True) &  (paired == False):    
+            t_real[var] = stats.ttest_ind(Y1[var,:],Y2[var,:])[0]
+        elif (parametric == True) &  (paired == True):    
+              t_real[var] = stats.ttest_rel(Y1[var,:],Y2[var,:])[0]
+        elif (parametric == False) &  (paired == False):    
+              t_real[var] = stats.mannwhitneyu(Y1[var,:],Y2[var,:])[0]
+        elif (parametric == False) &  (paired == True):    
+              t_real[var] = stats.wilcoxon(Y1[var,:],Y2[var,:])[0]   
+        
+        ###generate the cdf
+        mu[var]  = np.mean(statistic[var])
+        #sort the data:
+        data_sorted = np.sort(statistic[var])
+        
+        #calculate the proportional values of samples
+        p = 1. * np.arange(len(data_sorted)) / (len(data_sorted) - 1)    
+        
+        #Evaluate is the real statistic falls in the critical region of the cdf
+        if t_real[var] > mu[var]:
+            p_val[var] = 1 - p[np.argwhere(t_real[var] > data_sorted)[-1]]              
+        else:
+            p_val[var] = p[np.argwhere(t_real[var] < data_sorted)[0]]           
+    
+    return([t_real,mu,p_val,statistic,surrogates_mean,surrogates_std])
+
+
+def overallFC_permutation_test(Y1,Y2,X1,X2,permutations,iters,tolerance,
+                               parametric=True,paired=False):
     
     '''
     Permutation test to discard the effect of overall FC from between groups differences in topology.
@@ -260,15 +376,23 @@ if __name__=='__main__':
     #higher in group 1
     print(stats.ttest_ind(GE_Group_1_res.T,GE_Group_2_res.T))
     
-
-    ###Permutation test
-    t_real,mu,p_val,statistic,means,surr_std = permutation_test(GE_Group_1,GE_Group_2,
-                                                                Overall_FC_Group_1,
-                                                                Overall_FC_Group_2,
-                                                                10000,1000,0.01,
-                                                                parametric=True,paired=False)  
+    #Note: instead of using a t-test and the corrected metrics, you can also use the permutation test
+    #without the correction for Overall FC (you already corrected with the linear correction). This
+    #approach is recommended when groups have a small sample size.
+    ###Simple Permutation test
+    t_real,mu,p_val,statistic,means,surr_std = permutation_test(GE_Group_1_res,GE_Group_2_res,
+                                                                50000,parametric=True,paired=False)      
+    print(t_real,mu,p_val)
     
-    #The permutation test also suggests that global efficiency was higher in group 1.
+
+    ###Permutation test modified for discard the effect of the Overall FC
+    t_real,mu,p_val,statistic,means,surr_std = overallFC_permutation_test(GE_Group_1,GE_Group_2,
+                                                                          Overall_FC_Group_1,
+                                                                          Overall_FC_Group_2,
+                                                                          10000,1000,0.01,
+                                                                          parametric=True,paired=False)  
+    
+    #This permutation test also suggests that global efficiency was higher in group 1.
     #The real t statistic was higher than the average statistic overall surrogates,
     #and the p-val was also significant.
     print(t_real,mu,p_val)
